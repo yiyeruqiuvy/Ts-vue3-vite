60f300073f3ae3ceda222b9973851f11bdf193e3
peiqf <peiqf@yinhai.com>
2025-04-24 10:10:13 +0800

    本地通过8080代理访问

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,26 +1,28 @@
-import axios from 'axios';
-import { ElMessage } from 'element-plus';
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
-axios.defaults.timeout = 1000 * 60; //设置接口超时时间
+axios.defaults.timeout = 1000 * 60 //设置接口超时时间
 // axios.defaults.headers.post['Content-Type'] =
 //   'application/x-www-form-urlencoded; charset=UTF-8';
 axios.interceptors.request.use(
   (config) => {
     // // 暂时写死token
     // // eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlzcyI6Imx4a2oiLCJpYXQiOjE1NjYzNTU5NTQsImp0aSI6IjY2NjY2NiJ9.5MaKCpzwnwojjUJPowXjaMrz4apl3AOAW4oK0LD7vqo
+    // const TOKEN =
+    //   'eyJ0eXAiOiJqd3QiLCJjbGFnIjoiSFM1MTIiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiIxNzIuMjIuMS4xNCIsImlhdCI6MTc0NTI4NzE0OSwic3ViIjoiRTI1NkM4QTQ3RTMxOEI2RTYxNUU4QkI4MEM0OEMyQzIxMDhFNjIzNzZCM0E3QzUxM0MyNTMxRkYxNTYxQTcwMEU5MkI3MTE1MkM1OTdEQTE5N0ZGRkYyQzk0NERCMUREIiwianRpIjoiNDNmYmFlNzg2MzJmNGRmOTllMjI0YTlmNjYxZTdjNDUifQ.M5KN9XHm2YbMnd5NPozlr05dtpspyNE5Gfj8jOh29PgYBbk-fqw3UOSzzBjJScspVDtTWv5u4I_enBfc0dtL-A'
     const TOKEN =
-      'eyJ0eXAiOiJqd3QiLCJjbGFnIjoiSFM1MTIiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiIxNzIuMjIuMS4xNCIsImlhdCI6MTc0NTI4NzE0OSwic3ViIjoiRTI1NkM4QTQ3RTMxOEI2RTYxNUU4QkI4MEM0OEMyQzIxMDhFNjIzNzZCM0E3QzUxM0MyNTMxRkYxNTYxQTcwMEU5MkI3MTE1MkM1OTdEQTE5N0ZGRkYyQzk0NERCMUREIiwianRpIjoiNDNmYmFlNzg2MzJmNGRmOTllMjI0YTlmNjYxZTdjNDUifQ.M5KN9XHm2YbMnd5NPozlr05dtpspyNE5Gfj8jOh29PgYBbk-fqw3UOSzzBjJScspVDtTWv5u4I_enBfc0dtL-A';
+      'eyJ0eXAiOiJqd3QiLCJjbGFnIjoiSFM1MTIiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiIxNzIuMjUuMjIuMTMwIiwiaWF0IjoxNzQ1NDYwMTg5LCJzdWIiOiI1MUM3RjBERDZEMDcyNzM4RDVFQ0VFQjhFNjhCOTZERTI1NUFGOEI0RTNCRkEyNDA1MjI4ODY4M0RBNzczOUY5RTkyQjcxMTUyQzU5N0RBMTk3RkZGRjJDOTQ0REIxREQiLCJqdGkiOiJmMWFhZDllM2VlZTA0Y2RjYmI3MzAwOWUzNjg0N2VhZiJ9.Bq5hMpeO0DnS-pispqaywnTniV2PtOpFei7-V1kFMl7CfpHEFM5rPSRc14Mvs3WiOjyaIL_q0eyhhlyCRqHDcA'
     if (TOKEN) {
-      config.headers['TA-JTOKEN'] = TOKEN;
+      config.headers['TA-JTOKEN'] = TOKEN
       config.headers['Content-Type'] =
-        'application/x-www-form-urlencoded; charset=UTF-8';
+        'application/x-www-form-urlencoded; charset=UTF-8'
     }
-    return config;
+    return config
   },
   (error) => {
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 axios.interceptors.response.use(
   (response) => {
@@ -34,50 +36,50 @@ axios.interceptors.response.use(
             ElMessage.error('登录超时了,请重新登录');
         } */
     // return response.data;
-    return response;
+    return response
   },
   (error) => {
     //断网处理或者请求超时
-    console.log(error, '接口调用失败');
+    console.log(error, '接口调用失败')
     if (!error.response) {
       //请求超时
       if (error.ElMessage.includes('timeout')) {
-        ElMessage.error('请求超时了,请稍后重试');
+        ElMessage.error('请求超时了,请稍后重试')
       } else {
         //断网，可以展示断网组件
-        ElMessage.error('请检查网络连接是否正常');
+        ElMessage.error('请检查网络连接是否正常')
       }
-      return;
+      return
     }
-    const status = error.response.status;
+    const status = error.response.status
     switch (status) {
       case 500:
-        ElMessage.error('服务器内部错误');
-        break;
+        ElMessage.error('服务器内部错误')
+        break
       case 404:
-        ElMessage.error('未找到远程服务器');
-        break;
+        ElMessage.error('未找到远程服务器')
+        break
       case 400:
-        ElMessage.error('数据异常');
-        break;
+        ElMessage.error('数据异常')
+        break
       default:
-        ElMessage.error(error.response.data.ElMessage);
+        ElMessage.error(error.response.data.ElMessage)
     }
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
-export default axios;
+export default axios
 
 // 封装动态url
-let _hostname;
-let _protocol;
-let _port;
-let _hostname_sess = window.localStorage.getItem('_host');
-let _protocol_sess = window.localStorage.getItem('_protocol');
-let _port_sess = window.localStorage.getItem('_port');
-let url1: string = '';
-let modelUrl: string = '';
+let _hostname
+let _protocol
+let _port
+let _hostname_sess = window.localStorage.getItem('_host')
+let _protocol_sess = window.localStorage.getItem('_protocol')
+let _port_sess = window.localStorage.getItem('_port')
+let url1: string = ''
+let modelUrl: string = ''
 if (process.env.NODE_ENV == 'development') {
   // 开发环境
   /* const HOST_NAME = [
@@ -90,19 +92,20 @@ if (process.env.NODE_ENV == 'development') {
     _hostname = '172.17.11.59';
     _protocol = 'https:';
     _port = 8861; */
-  url1 = '/local/';
+  url1 = 'http://23.210.52.80:18081/cockpit/api'
   // modelUrl = 'http://23.210.52.80:18081/cockpit/api';
-  modelUrl = 'http://localhost:8080/cockpit1';
+  modelUrl = 'http://localhost:8080/cockpit1'
 } else if (process.env.NODE_ENV == 'production') {
-  const { hostname, protocol, port } = window.location;
+  const { hostname, protocol, port } = window.location
   // 生产环境
-  _hostname = _hostname_sess ? _hostname_sess : hostname;
-  _protocol = _protocol_sess ? _protocol_sess : protocol;
-  _port = _port_sess ? _port_sess : port;
-  url1 = `${_protocol}//${_hostname}:${_port}/`;
-  modelUrl = `${_protocol}//${_hostname}:${_port}/`;
+  _hostname = _hostname_sess ? _hostname_sess : hostname
+  _protocol = _protocol_sess ? _protocol_sess : protocol
+  _port = _port_sess ? _port_sess : port
+  url1 = `${_protocol}//${_hostname}:${_port}/cockpit/api`
+  // modelUrl = `${_protocol}//${_hostname}:${_port}/cockpit/api`
+  modelUrl = 'http://23.210.52.54:28081/cockpit/api'
 }
 
-console.log('设置之后的ip地址是', url1);
+console.log('设置之后的ip地址是', url1)
 
-export { url1, modelUrl };
+export { url1, modelUrl }
